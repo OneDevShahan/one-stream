7c8a5f385e3e7c54628dc95a1e0116524396d678
OneDevShahan <onedevshahan@gmail.com>
2025-01-02 19:44:06 +0530

    dev-release: Updating package.json file

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Tells Next.js to generate static HTML files during the build
-  trailingSlash: true, // Ensures paths end with a slash, which is needed for GitHub Pages
+  trailingSlash: true,
 };
 
 export default nextConfig;
